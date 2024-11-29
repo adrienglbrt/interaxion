74733d35fe92798b180fa5fc8b7565fa8a5f0ca9
Adrien <adrien.galibert@gmail.com>
2024-11-29 10:51:02 +0100

    Edit media config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.tina.io",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
