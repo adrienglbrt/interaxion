efb142af78c1fee54a7a619beb9a3fb2a55a0f5e
Adrien <adrien.galibert@gmail.com>
2024-11-30 19:01:25 +0100

    Create grid component

---
+++ src/components/layout/grid.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export default function Grid({
+  children,
+}: {
+  children: ReactNode | ReactNode[];
+}) {
+  return (
+    <div className='grid grid-cols-6 sm:grid-cols-12 gap-8'>{children}</div>
+  );
+}
