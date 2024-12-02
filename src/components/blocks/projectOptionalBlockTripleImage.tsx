05441acd66103e69d0cd5e0676b03779b20b4077
Adrien <adrien.galibert@gmail.com>
2024-12-02 17:39:12 +0100

    Fix triple image block layout

--- src/components/blocks/projectOptionalBlockTripleImage.tsx
+++ src/components/blocks/projectOptionalBlockTripleImage.tsx
@@ -18,30 +18,32 @@ export default function ProjectOptionalBlockTripleImage({
   } = block;
   return (
     <div className='pt-10 lg:pt-20 col-span-6 lg:col-span-12 grid grid-cols-6 lg:grid-cols-12 gap-1 sm:gap-2 lg:gap-4'>
-      <Image
-        src={imageOne}
-        alt={altOne ?? ""}
-        width={aspectRatioWidth}
-        height={aspectRatioHeight}
-        sizes='33vw'
-        className='col-span-2 lg:col-start-4 w-full'
-      />
-      <Image
-        src={imageTwo}
-        alt={altTwo ?? ""}
-        width={aspectRatioWidth}
-        height={aspectRatioHeight}
-        sizes='33vw'
-        className='col-span-2 w-full'
-      />
-      <Image
-        src={imageThree}
-        alt={altThree ?? ""}
-        width={aspectRatioWidth}
-        height={aspectRatioHeight}
-        sizes='33vw'
-        className='col-span-2 w-full'
-      />
+      <div className='col-span-6 lg:col-start-2 lg:col-span-10 flex gap-1 sm:gap-2 lg:gap-4'>
+        <Image
+          src={imageOne}
+          alt={altOne ?? ""}
+          width={aspectRatioWidth}
+          height={aspectRatioHeight}
+          sizes='33vw'
+          className='flex-1'
+        />
+        <Image
+          src={imageTwo}
+          alt={altTwo ?? ""}
+          width={aspectRatioWidth}
+          height={aspectRatioHeight}
+          sizes='33vw'
+          className='flex-1'
+        />
+        <Image
+          src={imageThree}
+          alt={altThree ?? ""}
+          width={aspectRatioWidth}
+          height={aspectRatioHeight}
+          sizes='33vw'
+          className='flex-1'
+        />
+      </div>
     </div>
   );
 }
