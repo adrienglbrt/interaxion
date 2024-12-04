88872904b7587b1bef7cfdbfe10fc1ffb0b394df
Adrien <adrien.galibert@gmail.com>
2024-12-04 12:54:38 +0100

    Fix animated underline link
    Added a white border instead of white background, so that there is not discontinuity of hover state in between the label and the underline

--- src/components/ui/animatedUnderlineLink.tsx
+++ src/components/ui/animatedUnderlineLink.tsx
@@ -10,13 +10,13 @@ export default function AnimatedUnderlineLink({
   className?: string;
 }) {
   return (
-    <div className='relative inline-block group h-8 overflow-hidden'>
+    <div className='group relative inline-block h-8 overflow-hidden'>
       <Link
         href={href}
         scroll={false}
         className={`
-          relative z-10 uppercase lg:text-xl text text-white 
-          transition-all duration-500 ease-in-out
+          relative uppercase lg:text-xl text text-white 
+          transition-all duration-500 ease-in-out cursor-pointer
           ${className}
         `}
       >
@@ -25,8 +25,8 @@ export default function AnimatedUnderlineLink({
         {/* Base Underline */}
         <span
           className='
-            absolute left-0 -bottom-1 h-[1px] w-full 
-            bg-white origin-left 
+            absolute left-0 top-full h-1 w-full 
+              border-white border-b
             transition-transform duration-700 ease-in-out
             group-hover:translate-x-[200%]
           '
@@ -35,8 +35,8 @@ export default function AnimatedUnderlineLink({
         {/* Hover Underline */}
         <span
           className='
-            absolute left-0 -bottom-1 h-[1px] w-full 
-            bg-white origin-right 
+            absolute left-0 top-full h-1 w-full 
+             border-white border-b
             transition-transform duration-700 ease-in-out
             transform -translate-x-[200%]
             group-hover:translate-x-0
