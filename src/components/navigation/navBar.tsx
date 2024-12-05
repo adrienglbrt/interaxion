f7c4008a3c6820698dc0ff72df10b4cf26ee305e
Adrien <adrien.galibert@gmail.com>
2024-12-05 18:15:57 +0100

    Add logo

--- src/components/navigation/navBar.tsx
+++ src/components/navigation/navBar.tsx
@@ -1,8 +1,10 @@
 import { useGlobal } from "@/utils/globalContext";
 import { useMobile } from "@/utils/mobileContext";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import Grid from "../layout/grid";
 import Wrapper from "../layout/wrapper";
+import Logo from "../ui/logo";
 import NavEntry from "./navEntry";
 
 export default function NavBar() {
@@ -24,8 +26,14 @@ export default function NavBar() {
     <nav className='fixed bottom-0 h-16 w-full mix-blend-difference text-white z-20'>
       <Wrapper>
         <Grid>
-          <div className='h-full flex items-center'>
-            <NavEntry href='/'>Interaxion</NavEntry>
+          <div className='col-span-2 h-full flex items-center'>
+            <Link
+              href='/'
+              className='uppercase hover:opacity-70 transition-opacity duration-300'
+              scroll={false}
+            >
+              <Logo className='h-[14px] fill-current' />
+            </Link>
           </div>
           <div className='relative h-full flex items-center justify-between col-start-4 lg:col-start-7 col-span-3 lg:col-span-6'>
             <NavEntry href='/work'>Work</NavEntry>
