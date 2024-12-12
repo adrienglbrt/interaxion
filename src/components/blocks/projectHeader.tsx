b56d8784d1e339aaa988e1146eabba18d74c4d86
Adrien <adrien.galibert@gmail.com>
2024-12-12 11:22:51 +0100

    Add main video player modal

--- src/components/blocks/projectHeader.tsx
+++ src/components/blocks/projectHeader.tsx
@@ -1,8 +1,10 @@
 import { VideoLinkObject } from "@/types/interfaces";
 import { getVideoLinkByRendition } from "@/utils/vimeoQueries";
 import Image from "next/image";
+import { useState } from "react";
 import { Project } from "../../../tina/__generated__/types";
 import Wrapper from "../layout/wrapper";
+import VideoMainPlayer from "../ui/videoMainPlayer";
 
 export default function ProjectHeader({
   project,
@@ -11,11 +13,15 @@ export default function ProjectHeader({
   project: Project;
   mainVideoDirectLinks: VideoLinkObject[];
 }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const mainVideoSrc = getVideoLinkByRendition(
     mainVideoDirectLinks ?? [],
     "adaptive"
   );
-  console.log(mainVideoSrc);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <header className='relative h-screen w-full'>
       <Image
@@ -26,6 +32,17 @@ export default function ProjectHeader({
         sizes='100vw'
         className='object-cover'
       />
+
+      <div className='absolute inset-0 flex items-center justify-center'>
+        <button
+          onClick={openModal}
+          className='bg-white bg-opacity-50 hover:bg-opacity-75 text-black p-4 rounded-full transition-all duration-300 ease-in-out'
+          aria-label='Play video'
+        >
+          Play
+        </button>
+      </div>
+
       <div className='h-full w-full'>
         <Wrapper>
           <div className='sticky top-16 pb-16 mix-blend-difference'>
@@ -36,6 +53,12 @@ export default function ProjectHeader({
           </div>
         </Wrapper>
       </div>
+
+      {isModalOpen && mainVideoSrc && (
+        <div className='fixed inset-0 flex items-center justify-center bg-black z-[100]'>
+          <VideoMainPlayer mainVideoSrc={mainVideoSrc} onClose={closeModal} />
+        </div>
+      )}
     </header>
   );
 }
---
+++ src/components/ui/videoMainPlayer.tsx
@@ -0,0 +1,186 @@
+import Hls from "hls.js";
+import { useEffect, useRef, useState } from "react";
+
+export default function VideoMainPlayer({
+  mainVideoSrc,
+  onClose,
+}: {
+  mainVideoSrc: string;
+  onClose: () => void;
+}) {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [isPlaying, setIsPlaying] = useState(true);
+  const [isMuted, setIsMuted] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  useEffect(() => {
+    if (videoRef.current && mainVideoSrc) {
+      const videoElement = videoRef.current;
+
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+
+        hls.on(Hls.Events.ERROR, (event, data) => {
+          if (data.fatal) {
+            console.error("Fatal HLS error:", data);
+          }
+        });
+
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+          console.log("HLS manifest parsed, ready to play");
+          videoElement
+            .play()
+            .catch((error) => console.error("Autoplay failed:", error));
+        });
+
+        hls.loadSource(mainVideoSrc);
+        hls.attachMedia(videoElement);
+
+        return () => {
+          hls.destroy();
+        };
+      } else if (videoElement.canPlayType("application/vnd.apple.mpegurl")) {
+        videoElement.src = mainVideoSrc;
+        videoElement
+          .play()
+          .catch((error) => console.error("Autoplay failed:", error));
+      }
+    }
+  }, [mainVideoSrc]);
+
+  const togglePlay = () => {
+    if (videoRef.current) {
+      if (isPlaying) {
+        videoRef.current.pause();
+      } else {
+        videoRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  const toggleMute = () => {
+    if (videoRef.current) {
+      videoRef.current.muted = !isMuted;
+      setIsMuted(!isMuted);
+    }
+  };
+
+  const handleProgress = () => {
+    if (videoRef.current) {
+      const currentTime = videoRef.current.currentTime;
+      const duration = videoRef.current.duration;
+      setProgress((currentTime / duration) * 100);
+    }
+  };
+
+  const handleSeek = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (videoRef.current) {
+      const newTime =
+        (parseFloat(event.target.value) / 100) * videoRef.current.duration;
+      videoRef.current.currentTime = newTime;
+    }
+  };
+
+  const toggleFullscreen = () => {
+    if (!document.fullscreenElement) {
+      containerRef.current?.requestFullscreen();
+      setIsFullscreen(true);
+    } else {
+      document.exitFullscreen();
+      setIsFullscreen(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleFullscreenChange = () => {
+      setIsFullscreen(!!document.fullscreenElement);
+    };
+
+    document.addEventListener("fullscreenchange", handleFullscreenChange);
+
+    return () => {
+      document.removeEventListener("fullscreenchange", handleFullscreenChange);
+    };
+  }, []);
+
+  const styles = `
+  input[type="range"]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 0;
+    height: 0;
+  }
+  input[type="range"]::-moz-range-thumb {
+    width: 0;
+    height: 0;
+    border: none;
+  }
+`;
+
+  return (
+    <div className='relative w-full h-full' ref={containerRef}>
+      <div onClick={togglePlay}>
+        <video
+          ref={videoRef}
+          loop
+          muted={isMuted}
+          playsInline
+          autoPlay
+          preload='auto'
+          className='absolute w-full h-full object-contain'
+          onTimeUpdate={handleProgress}
+        >
+          <source src={mainVideoSrc} type='application/vnd.apple.mpegurl' />
+          <source src={mainVideoSrc} type='video/mp4' />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <style>{styles}</style>
+      <div className='absolute bottom-4 left-4 right-4 flex items-center space-x-4 bg-black p-2 rounded'>
+        <button
+          onClick={togglePlay}
+          className='text-white bg-grey p-2 rounded focus:outline-none'
+          aria-label={isPlaying ? "Pause" : "Play"}
+        >
+          {isPlaying ? "Pause" : "Play"}
+        </button>
+        <button
+          onClick={toggleMute}
+          className='text-white bg-grey p-2 rounded focus:outline-none'
+          aria-label={isMuted ? "Unmute" : "Mute"}
+        >
+          {isMuted ? "Unmute" : "Mute"}
+        </button>
+        <input
+          type='range'
+          min='0'
+          max='100'
+          value={progress}
+          onChange={handleSeek}
+          className='flex-grow h-1 appearance-none bg-grey outline-none'
+          style={{
+            background: `linear-gradient(to right, white ${progress}%, #585858 ${progress}%)`,
+          }}
+        />
+        <button
+          onClick={toggleFullscreen}
+          className='text-white bg-grey p-2 rounded focus:outline-none'
+          aria-label={isFullscreen ? "Exit Fullscreen" : "Enter Fullscreen"}
+        >
+          {isFullscreen ? "Exit Fullscreen" : "Fullscreen"}
+        </button>
+      </div>
+
+      <button
+        onClick={onClose}
+        className='absolute top-4 right-4 text-white bg-grey p-2 rounded-full focus:outline-none'
+        aria-label='Close video'
+      >
+        Close
+      </button>
+    </div>
+  );
+}
