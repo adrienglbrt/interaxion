7835fb41f347158647436f96b4e3f97f527a1970
Adrien <adrien.galibert@gmail.com>
2024-12-16 17:13:03 +0100

    Add scoll animation on image in about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,9 +6,11 @@ import RichText from "@/components/ui/richText";
 import { PageProps } from "@/types/interfaces";
 import { getGlobalData, getPageData } from "@/utils/dataQueries";
 import { useGlobal } from "@/utils/globalContext";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { GetStaticProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import { useRef } from "react";
 import { useTina } from "tinacms/dist/react";
 import { PageAbout } from "../../tina/__generated__/types";
 
@@ -21,6 +23,12 @@ export default function About({ pageData }: { pageData: PageProps }) {
   const page = data.page as PageAbout;
   const globalData = useGlobal();
   const { contact, socials } = globalData.data.global;
+
+  const containerRef = useRef(null);
+  const { scrollYProgress } = useScroll();
+
+  // Create a translation effect based on scroll
+  const y = useTransform(scrollYProgress, [0, 1], [0, 200]);
   return (
     <>
       <MetaTags
@@ -34,15 +42,20 @@ export default function About({ pageData }: { pageData: PageProps }) {
             <div className='col-span-6 sm:col-start-2 sm:col-span-4 lg:col-start-7 lg:col-span-5 pt-20 lg:pt-40'>
               <RichText text={page.introductionText} />
             </div>
-            <div className='col-span-6 lg:col-span-12 -mx-4 sm:-mx-8 lg:-mx-16 pt-10 lg:pt-20'>
-              <div className='relative pb-[140%] sm:pb-[56.25%]'>
-                <Image
-                  src={page.image}
-                  alt={page.imageAlt ?? ""}
-                  fill
-                  sizes='100vw'
-                  className='object-cover'
-                />
+            <div
+              className='col-span-6 lg:col-span-12 -mx-4 sm:-mx-8 lg:-mx-16 pt-10 lg:pt-20'
+              ref={containerRef}
+            >
+              <div className='relative pb-[140%] sm:pb-[56.25%] overflow-hidden'>
+                <motion.div style={{ y }} className='absolute inset-0'>
+                  <Image
+                    src={page.image}
+                    alt={page.imageAlt ?? ""}
+                    fill
+                    sizes='100vw'
+                    className='object-cover scale-105'
+                  />
+                </motion.div>
               </div>
             </div>
             <div className='col-span-6 sm:col-start-2 sm:col-span-4 lg:col-start-7 lg:col-span-5 pt-10 lg:pt-20'>
