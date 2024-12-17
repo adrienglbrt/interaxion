409f33debc1d41439f7801e5faaa2381be09bd7b
Adrien <adrien.galibert@gmail.com>
2024-12-17 11:07:55 +0100

    Tweak animated underline link

--- src/components/ui/animatedUnderlineLink.tsx
+++ src/components/ui/animatedUnderlineLink.tsx
@@ -12,20 +12,20 @@ export default function AnimatedUnderlineLink({
       <Link
         href={href}
         scroll={false}
-        className='relative transition-all duration-500 ease-in-out cursor-pointer'
+        className='relative transition-opacity duration-500 hover:opacity-80 ease-in-out cursor-pointer'
       >
         {children}
 
         {/* Base Underline */}
         <span
           className='
-            absolute left-0 top-full h-[0.125rem] w-full border-black border-b transition-transform duration-700 ease-in-out group-hover:translate-x-[200%]'
+            absolute left-0 top-full h-[0.125rem] w-full border-black border-b transition-transform duration-1000 ease-in-out group-hover:translate-x-[200%]'
         />
 
         {/* Hover Underline */}
         <span
           className='
-            absolute left-0 top-full h-[0.125rem] w-full border-black border-b transition-transform duration-700 ease-in-out transform -translate-x-[200%] group-hover:translate-x-0'
+            absolute left-0 top-full h-[0.125rem] w-full border-black border-b transition-transform duration-1000 ease-in-out transform -translate-x-[200%] group-hover:translate-x-0'
         />
       </Link>
     </div>
