d73990a3d336eabecd4d52e850295ffdfa18462a
Adrien <adrien.galibert@gmail.com>
2024-12-17 11:10:17 +0100

    Created dedicated animated text component

--- src/components/ui/showcaseItem.tsx
+++ src/components/ui/showcaseItem.tsx
@@ -1,46 +1,12 @@
 import { ProjectWithDirectLinks } from "@/types/interfaces";
 import { useMobile } from "@/utils/mobileContext";
 import { getVideoLinkByRendition } from "@/utils/vimeoQueries";
-import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import { useMemo } from "react";
 import Wrapper from "../layout/wrapper";
+import AnimatedText from "./animatedText";
 import VideoLoop from "./videoLoop";
-
-const AnimatedText = ({
-  text,
-  isVisible,
-}: {
-  text: string;
-  isVisible: boolean;
-}) => {
-  return (
-    <span className='inline-block'>
-      {text.split("").map((char, index) => (
-        <span
-          key={`${text}-${index}`}
-          className='inline-block overflow-hidden'
-          style={{ verticalAlign: "top" }}
-        >
-          <motion.span
-            className='inline-block pb-1 sm:pb-2 lg:pb-3'
-            initial={{ y: "100%" }}
-            animate={{ y: isVisible ? 0 : "100%" }}
-            transition={{
-              duration: 0.8,
-              // ease: [0.33, 1, 0.68, 1],
-              ease: [0.79, 0.14, 0.15, 0.86],
-              delay: index * 0.03, // Staggered delay
-            }}
-          >
-            {char === " " ? "\u00A0" : char}
-          </motion.span>
-        </span>
-      ))}
-    </span>
-  );
-};
 
 export default function ShowcaseItem({
   project,
